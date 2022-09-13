bace8fc764b5b471c8db6f309f6c0ede943d4b57
muleysteve <30406388+muleysteve@users.noreply.github.com>
2022-09-13 12:30:31 +0100

    feature/checkstyle (#2)
    * Renamed workflow
    
    * Add build-helper and check-style plugins
    
    * Checkstyle changes
    
    Co-authored-by: Steve Roberts <steve.roberts@essarsoftware.co.uk>

--- src/main/java/essar/SimpleClass.java
+++ src/main/java/essar/SimpleClass.java
@@ -3,24 +3,24 @@ package essar;
 /**
  * This is a test class being used to showcase the process.
  */
-public class SimpleClass
-{
-	/**
-	 * Creates a new SimpleClass.
-	 */
-	public SimpleClass() {
+public class SimpleClass {
 
-		// Empty constructor
+    /**
+     * Creates a new SimpleClass.
+     */
+    public SimpleClass() {
 
-	}
+        // Empty constructor
 
-	/**
-	 * Says hello because it's nice to be nice.
-	 * @return a polite greeting.
-	 */
-	public String sayHello() {
+    }
 
-		return "Hello there!";
+    /**
+     * Says hello because it's nice to be nice.
+     * @return a polite greeting.
+     */
+    public String sayHello() {
 
-	}
+        return "Hello there!";
+
+    }
 }
